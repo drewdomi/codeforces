8b58b46a9db76531dc1f0f851f29019eb42deb6d
Andrew Domingues <contact@drewdomi.com>
2025-11-15 00:05:44 +0000

    feat: setup template

---
+++ problems/_template/template.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	res := Solution("something here")
+
+	fmt.Println(res)
+}
+
+func Solution(someParameter any) any {
+
+	return someParameter
+}
